4011d44d2924d290df65f2fb6f9aa62a6e8436ba
Appiah Boadu Prince Kwabena <appiahboaduprince@gmail.com>
2023-01-21 12:41:51 +0000

    upd: change URL tablename and key column name to custom_url
    upd: add is_admin column to users

--- turtle_link_shortener/models.py
+++ turtle_link_shortener/models.py
@@ -2,13 +2,14 @@ from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
 from . import database
 
 
-class UserURLS(database.Base):
+class UserURL(database.Base):
     __tablename__ = "user_url"
 
-    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"), primary_key=True, nullable=False)
+    link_created = Column(
+        String, ForeignKey("url.custom_url"), primary_key=True, nullable=False
+    )
 
-    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
-    link_created = Column(String, ForeignKey("url.key"), nullable=False)
     link_time_created = Column(DateTime, nullable=False)
 
     link_creator = database.relationship(
@@ -20,12 +21,12 @@ class UserURLS(database.Base):
     )
 
 
-class URLS(database.Base):
-    __tablename__ = "url"
+class URL(database.Base):
+    __tablename__ = "urls"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
 
-    key = Column(String, unique=True, nullable=False, index=True)
+    custom_url = Column(String, unique=True, nullable=False, index=True)
     secret_key = Column(String, unique=True, nullable=False, index=True)
     target_url = Column(String, nullable=False, index=True)
 
@@ -33,19 +34,20 @@ class URLS(database.Base):
     time_created = Column(DateTime, nullable=False)
     clicks = Column(Integer, default=0)
 
-    links_created: list[UserURLS] = database.relationship(
-        "UserURLS", back_populates="created_links", lazy="selectin"
+    links_created: list[UserURL] = database.relationship(
+        "UserURL", back_populates="created_links", lazy="selectin"
     )
 
 
 class User(database.Base):
     __tablename__ = "user"
 
-    id = Column(Integer, primary_key=True)
-    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
     username = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
+    is_admin = Column(Boolean, default=False)
 
-    links: list[UserURLS] = database.relationship(
-        "UserURLS", back_populates="link_creator", lazy="selectin"
+    links: list[UserURL] = database.relationship(
+        "UserURL", back_populates="link_creator", lazy="selectin"
     )
