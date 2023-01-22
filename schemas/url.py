2e92fd84a7b62e8f5e3e0cf6814f7c25398edf9a
Appiah Boadu Prince Kwabena <appiahboaduprince@gmail.com>
2023-01-22 01:43:07 +0000

    upd: update schema for url

--- schemas/url.py
+++ schemas/url.py
@@ -1,15 +1,21 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 
 class URLBase(BaseModel):
-    pass
-
-
-class URLShortener(URLBase):
-    pass
+    target_url: str
 
 
 class URL(URLBase):
+    is_active: bool
+    time_created: datetime
+    clicks: int
 
     class Config:
         orm_mode = True
+
+
+class URLInfo(URL):
+    custom_url: str
+    admin_url: str
+    
