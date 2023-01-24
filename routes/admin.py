343f8e13d4796c5e2b20166f1c801fce40b491fa
Appiah Boadu Prince Kwabena <appiahboaduprince@gmail.com>
2023-01-24 11:57:41 +0000

    upd: change create post async

--- routes/admin.py
+++ routes/admin.py
@@ -1,3 +1,20 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, Depends
+from schemas.user import AdminCreate
+from utils.database_utils import get_db
+from sqlalchemy.orm import Session
+from turtle_link_shortener.models import User as UserModel
 
-admin = FastAPI()
+admin = APIRouter()
+
+
+@admin.post("/admin/create", tags=["admin"])
+async def create_admin(
+    new_admin: AdminCreate, db: Session = Depends(get_db)
+):
+    db_admin = UserModel(**new_admin.dict())
+
+    db.add(db_admin)
+    db.commit()
+    db.refresh(db_admin)
+
+    return db_admin
