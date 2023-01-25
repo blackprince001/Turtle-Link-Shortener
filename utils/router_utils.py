bf2db0153fc74ab41adf882a8547bd4df7ccf1ad
Appiah Boadu Prince Kwabena <appiahboaduprince@gmail.com>
2023-01-25 14:06:55 +0000

    chore: add custom key url generator utility functions

---
+++ utils/router_utils.py
@@ -0,0 +1,16 @@
+import secrets
+from string import ascii_letters as letters, digits
+
+def rand_char() -> str:
+    return secrets.choice(letters + digits)
+
+
+def generate_keys(custom: str = ""):
+    if custom == "":
+        key = "".join(rand_char() for _ in range(3)) + "-" + "".join(
+            rand_char() for _ in range(3)
+        ) + "-" + "".join(rand_char() for _ in range(3))
+        custom = key
+        
+    secret_key = "".join(rand_char() for _ in range(16))
+    return (custom, secret_key)
